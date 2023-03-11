8fc75d0903e16bcb94670d23a6b09430cd87c997
Ismael Nicolas Cedillo Paz <ismael.cedillo@outlook.es>
2023-03-11 17:07:48 -0500

    creation of middelware to handle routes

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from routes.auth import auth
 from routes.client import client
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import HTMLResponse
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(auth)
 app.include_router(client)
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+async def client_middleware(request: Request, call_next):
+    response = await call_next(request)
+    if response.status_code == 404:
+        file_path = "./static/index.html"
+        content = open(file_path, "r").read()
+        return HTMLResponse(content=content)
+    return response
+
+app.middleware("https")(client_middleware)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
