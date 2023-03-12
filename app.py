30217b7cd6ee2929c1e93104ce61e67d2dfc9522
Ismael Nicolas Cedillo Paz <ismael.cedillo@outlook.es>
2023-03-11 23:52:51 -0500

    update middelware

--- app.py
+++ app.py
@@ -23,15 +23,16 @@ app.include_router(client)
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 def not_found(request: Request):
-    with open("index.html", "r") as f:
+    with open("static/index.html", "r") as f:
         content = f.read()
     return HTMLResponse(content=content)
 
 @app.middleware("http")
-async def not_found(request: Request):
-    with open("static/index.html", "r") as f:
-        content = f.read()
-    return HTMLResponse(content=content)
+async def custom_not_found(request: Request, call_next):
+    response = await call_next(request)
+    if response.status_code == 404:
+        return not_found(request)
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=2303)
