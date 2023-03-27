654c3a26c1e897f9a552beb96c020a21abe7adeb
Ismael Nicolas Cedillo Paz <ismael.cedillo@outlook.es>
2023-03-27 09:29:58 -0500

    Change Search

--- routes/client.py
+++ routes/client.py
@@ -79,7 +79,7 @@ async def get_client_by_lastname(lastname:str,request: Request):
             cookie = jwt.decode(cookie,SECRET_KEY,algorithms=['HS256'])
             id_employee = cookie['iss']
 
-            sql = "SELECT c.ID_CLIENT,c.NAME_CLIENT,c.LASTNAME_CLIENT,s.DATE_SUBSCRIBE FROM CLIENT AS c INNER JOIN SUBSCRIBE AS s ON c.ID_CLIENT = s.ID_CLIENT WHERE  AND c.LASTNAME_CLIENT LIKE %s" 
+            sql = "SELECT c.ID_CLIENT,c.NAME_CLIENT,c.LASTNAME_CLIENT,s.DATE_SUBSCRIBE FROM CLIENT AS c INNER JOIN SUBSCRIBE AS s ON c.ID_CLIENT = s.ID_CLIENT WHERE  c.LASTNAME_CLIENT LIKE %s" 
             cursor.execute(sql, ( f'%{lastname}%'))
             answer = cursor.fetchall()
             
