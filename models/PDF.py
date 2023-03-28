44a2b551333dff370581b35ba7e0d6fdc9040249
Ismael Nicolas Cedillo Paz <ismael.cedillo@outlook.es>
2023-03-28 10:56:53 -0500

    Update report PDF

--- models/PDF.py
+++ models/PDF.py
@@ -15,17 +15,20 @@ class PDF(FPDF):
 
     def content(self, data):
         # Define el contenido del documento
-        self.set_font('Arial', '', 12)
+        self.set_font('Times', '', 12)
         for item in data:
             self.cell(20)
             self.cell(0, 10, txt="-" + item)
             self.ln(10)
 
 
+
 class ReportStages(FPDF):
     def __init__(self):
         super().__init__()
         self.add_page()
+        self.x_pdf = 0 
+
 
     def header(self):
     # Define el encabezado del documento
@@ -40,6 +43,7 @@ class ReportStages(FPDF):
     def content(self, cliente, cedula, asesor, etapas):
         # Definir el contenido
         # print(etapas)
+        self.set_right_margin(23)
         documents = [
             "3 Últimos Roles de pago",
             "1 Referencia Personal",
@@ -50,22 +54,8 @@ class ReportStages(FPDF):
         part2 = f'En el proceso para la entrega de su Carta de presentación y Económica, deberá enviar vía email: {asesor}, la siguiente documentación:'
 
         part3 = "En caso de que las fechas a la asistencia del proceso se establezcan días domingos y feriados, será programado para el siguiente día hábil."
-        # Agregar cliente y fecha
-        self.set_font('Arial', 'B', 12)  # cambiar a fuente en negrita
-        self.cell(40, 10, 'Cliente:', 0, 0)
-        self.set_font('Arial', '', 12)
-        self.cell(-10)
-        self.cell(0, 10, cliente, 0, 1)
-
-        self.set_font('Arial', 'B', 12)  # cambiar a fuente en negrita
-        self.cell(40, 10, 'Hora:', 0, 0)
-        self.set_font('Arial', '', 12)
-        self.cell(-10)
-        self.cell(0, 10, str(etapas[0]["stage_end_date"]).split()[1], 0, 1)
-        # part1 = f'Estimado/a cliente {cliente}, con cédula número {cedula}. Nos comunicamos de parte de Consorcio Acción para informarle que necesitamos recibir cierta documentación de su parte para poder continuar brindándole nuestros servicios de manera efectiva. Como parte de nuestros procedimientos internos, necesitamos que nos proporcione los siguientes documentos:\n'
-        self.ln()
-
-        # self.multi_cell(0, 10, part2)
+        
+        self.set_font('Times', '', 12)
 
         max_name_width = self.get_max_width(etapas, 'name_stage')
         #max_start_width = self.get_max_width(etapas, 'stage_start_date')
@@ -79,51 +69,61 @@ class ReportStages(FPDF):
         col_duration_width = max_duration_width + 6
 
         # Definir la altura de fila
-        row_height = self.font_size * 1.5
+        row_height = self.font_size * 2
 
         # Calcular la posición x para centrar la tabla en la página
         page_width = self.w
         table_width = col_name_width + col_end_width + col_duration_width
-        x = (page_width - table_width) / 2
+        self.x_pdf = (page_width - table_width) / 2
+
+        self.set_xy(self.x_pdf, self.y)
+        # Agregar cliente y fecha
+        self.set_font('Times', 'B', 12)  # cambiar a fuente en negrita
+        self.cell(40, 10, 'Cliente:', 0, 0)
+        self.set_font('Times', '', 12)
+        self.cell(-10)
+        self.cell(0, 10, cliente, 0, 1)
+
+        self.set_xy(self.x_pdf, self.y)
+        self.set_font('Times', 'B', 12)  # cambiar a fuente en negrita
+        self.cell(40, 10, 'Hora:', 0, 0)
+        self.set_font('Times', '', 12)
+        self.cell(-10)
+        self.cell(0, 10, str(etapas[0]["stage_end_date"]).split()[1], 0, 1)
+
+        self.ln()
+
         # Imprimir la tabla
         self.set_font('Arial', 'BI', 12)
         # Establecer la posición x para centrar la tabla
-        self.set_xy(self.x, self.y)
-        self.cell(col_name_width, row_height, 'Etapa', border='B')
-        #self.cell(col_start_width, row_height, 'Fecha inicio', border=1)
+        self.set_xy(self.x_pdf, self.y)
         self.cell(col_end_width, row_height, 'Fecha reunión', border='B')
+        self.cell(col_name_width, row_height, 'Etapa', border='B')
         self.cell(col_duration_width, row_height, 'Asistencia', border='B')
-        # self.cell(col_duration_width, row_height, 'Duración', border=1)
-        self.set_font('Arial', '', 12)
+
+        self.set_font('Times', 'I', 12)
 
         # Establecer el color de fondo
         r,g,b = 249,229,213
         step = 1 / 6
-
+        BORDER = 'T'
+        ASISTANCE = 'Vía Email'
 
         primera_etapa = True
 
         self.ln()
         for etapa in etapas:
             self.set_fill_color(r,g,b)
-            self.set_xy(self.x, self.y)
+            self.set_xy(self.x_pdf, self.y)
+
+            self.cell(col_end_width, row_height, str(etapa['stage_end_date']).split()[0], border=BORDER, fill=True)
+            self.cell(col_name_width, row_height,etapa['name_stage'], border=BORDER, fill=True)
+            self.cell(col_duration_width, row_height,ASISTANCE, border=BORDER, fill=True)
+
             if primera_etapa:
-
-                self.cell(col_name_width, row_height,etapa['name_stage'], border='T', fill=True)
-
-                self.cell(col_end_width, row_height, str(etapa['stage_end_date']).split()[0], border='T', fill=True)
-
-                self.cell(col_duration_width, row_height,
-                          "Email", border='T', fill=True)
-                
+                BORDER = 0
+                ASISTANCE = "Presencial"
                 primera_etapa = False
-            else:
-
-                self.cell(col_name_width, row_height,etapa['name_stage'], border=0, fill=True)
-
-                self.cell(col_end_width, row_height, str( etapa['stage_end_date']).split()[0], border=0, fill=True)
-
-                self.cell(col_duration_width, row_height,"Presencial", border=0, fill=True)
 
             self.ln()
 
@@ -131,33 +131,35 @@ class ReportStages(FPDF):
             g -= round(158 * step)
             b -= round(209 * step)
             
-        self.set_font('Arial', '', 12)
+        self.set_font('Times', '', 12)
 
         self.ln()
+        self.set_xy(self.x_pdf, self.y)
         self.multi_cell(0, 10, part2)
-        self.set_font('Arial', '', 12)
         # self.multi_cell(0, 10, part1)
 
         for item in documents:
+            self.set_xy(self.x_pdf, self.y)
             self.cell(10)
             self.cell(0, 10, txt="- " + item)
             self.ln(10)
 
+        self.set_xy(self.x_pdf, self.y)
         self.multi_cell(0, 10, part3)
 
 
     def footer(self):
         # Definir posición x e y
         self.set_y(-30)
-        self.set_x(10)
+        self.set_x(self.x_pdf)
 
         # Imprimir la fila de firma
-        self.set_font('Arial', '', 12)
+        self.set_font('Times', '', 12)
         self.cell(0, 10, 'Firma: _______________________', 0, 0, 'L')
 
         # Definir posición x e y para la identificación
         self.set_y(-20)
-        self.set_x(10)
+        self.set_x(self.x_pdf)
 
         # Imprimir la fila de identificación
         self.cell(0, 10, 'CI: __________________________', 0, 0, 'L')
