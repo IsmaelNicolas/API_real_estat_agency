3b3ba1c0a08836b9d0c4ac7a695a2cdd97595263
Ismael Nicolas Cedillo Paz <ismael.cedillo@outlook.es>
2023-03-29 07:24:21 -0500

    test insert data

--- routes/client.py
+++ routes/client.py
@@ -106,9 +106,8 @@ async def insert_data_client(client: InsertEconomicData, request: Request):
     try:
         conn = connection()
         with conn.cursor() as cursor:
-            sql = "UPDATE CLIENT SET SPOUSE_NAME = %s, SPOUSE_OCUPATION = %s, SPOUSE_DIRECTION = %s, SPOUSE_SALARY = %s, SPOUSE_ENTITY = %s, OCUPATION_CLIENT = %s, SALARY_CLIENT = %s, ENTITY_CLIENT = %s, DIRECTION_ENTITY = %s CLIENT_POSITION = %s WHERE ID_CLIENT = %s"
-            values = (client.spouse_lastname, client.spouse_ocupation, client.spouse_direction, client.spouse_salary, client.spouse_entity,
-                      client.client_ocupation, client.client_salary, client.client_entity, client.entity_direction, client.client_position,client.id_client)
+            sql = "UPDATE CLIENT SET SPOUSE_NAME = %s, SPOUSE_OCUPATION = %s, SPOUSE_DIRECTION = %s, SPOUSE_SALARY = %s, SPOUSE_ENTITY = %s, OCUPATION_CLIENT = %s, SALARY_CLIENT = %s, ENTITY_CLIENT = %s, DIRECTION_ENTITY = %s ,CLIENT_POSITION = %s WHERE ID_CLIENT = %s"
+            values = (client.spouse_lastname, client.spouse_ocupation, client.spouse_direction, client.spouse_salary, client.spouse_entity,client.client_ocupation, client.client_salary, client.client_entity, client.entity_direction, client.client_position,client.id_client)
             cursor.execute(sql, values)
         conn.commit()
 
